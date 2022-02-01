dc4208447454c530d0b8068cf69686ea9df75347
evguevarra <evguevarra08@gmail.com>
2022-02-01 20:45:40 +0800

    organized file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:fit_hub/login-screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_hub/screens/login-screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
